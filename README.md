f8b141ec27ed6534349945c9f9932168c7eb4bce
Andrew Hopkins <andhop@amazon.com>
2019-09-03 18:33:14 -0700

    Add instructions to build seperate branch of SDK, configure CRT HTTP client, make PQ TLS calls

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ Demo code to setup the Java SDK 2.0 to use Post-quantum TLS with KMS. See [this 
 ### Running the example
 ```$bash
 $ git clone https://github.com/aws-samples/aws-kms-pq-tls-example.git
-$ cd aws-kms-pq-tls-example
+$ git clone https://github.com/alexw91/aws-sdk-java-v2.git --branch aws_crt
+$ # TODO use this instead git clone https://github.com/aws/aws-sdk-java-v2.git --branch aws-crt-dev-preview
+$ cd aws-sdk-java-v2
+$ mvn clean install -P quick
+$ cd ../aws-kms-pq-tls-example
 $ mvn package
 $ java -jar target/aws-kms-pq-tls-example-1.0-jar-with-dependencies.jar
 ```
--- src/main/java/com/example/AwsKmsPqTlsExample.java
+++ src/main/java/com/example/AwsKmsPqTlsExample.java
@@ -18,16 +18,56 @@
 
 package com.example;
 
+import software.amazon.awssdk.crt.io.ClientBootstrap;
+import software.amazon.awssdk.crt.io.SocketOptions;
+import software.amazon.awssdk.crt.io.TlsCipherPreference;
+import software.amazon.awssdk.crt.io.TlsContext;
+import software.amazon.awssdk.crt.io.TlsContextOptions;
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
 import software.amazon.awssdk.services.kms.KmsAsyncClient;
 import software.amazon.awssdk.services.kms.model.KeyListEntry;
 import software.amazon.awssdk.services.kms.model.ListKeysResponse;
+import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
+import software.amazon.awssdk.utils.Logger;
 
 public class AwsKmsPqTlsExample {
+    private static final Logger log = Logger.loggerFor(AwsKmsPqTlsExample.class);
+
     public static void main(String[] args) throws Exception {
-        KmsAsyncClient kms = KmsAsyncClient.builder().build();
-        final ListKeysResponse response = kms.listKeys().get();
-        for(KeyListEntry key : response.keys()){
-            System.out.println(key.keyArn());
+
+        TlsCipherPreference cipherPreferences = null;
+        if (TlsContextOptions.isCipherPreferenceSupported(TlsCipherPreference.TLS_CIPHER_KMS_PQ_TLSv1_0_2019_06)) {
+            log.info(() -> "Post-quantum ciphers are supported and will be used!");
+            cipherPreferences = TlsCipherPreference.TLS_CIPHER_KMS_PQ_TLSv1_0_2019_06;
+        } else {
+            log.warn(() -> "Post-quantum ciphers are not supported, falling back to classic cipher suites.");
+            cipherPreferences = TlsCipherPreference.TLS_CIPHER_SYSTEM_DEFAULT;
+        }
+        try (ClientBootstrap bootstrap = new ClientBootstrap(1)) {
+            try (SocketOptions socketOptions = new SocketOptions()) {
+                try (TlsContextOptions contextOptions = new TlsContextOptions().withCipherPreference(cipherPreferences)) {
+                    try (TlsContext tlsContext = new TlsContext(contextOptions)) {
+                        try (SdkAsyncHttpClient awsCrtHttpClient = AwsCrtAsyncHttpClient.builder()
+                                .bootstrap(bootstrap)
+                                .socketOptions(socketOptions)
+                                .tlsContext(tlsContext)
+                                .build()) {
+                            try (KmsAsyncClient asyncKMSClient = KmsAsyncClient.builder()
+                                    .httpClient(awsCrtHttpClient)
+                                    .build()) {
+
+                                final ListKeysResponse response = asyncKMSClient.listKeys().get();
+                                log.info(() -> String.format("Found %d keys and %s have more pages.",
+                                        response.keys().size(), response.truncated()? "do" : "do not"));
+                                
+                                for(KeyListEntry key : response.keys()){
+                                    log.info(() -> key.keyArn());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
